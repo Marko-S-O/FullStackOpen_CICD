c371514822c4bf35a2fffc8d2f314e56f9582509
Marko-S-O <rek@orasaari.com>
2024-10-01 23:47:33 +0300

    Add files via upload

--- phonebook-frontend/vite.config.js
+++ phonebook-frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures output is in ./dist directory
+    emptyOutDir: true, // Optionally clears the directory before building
+  },
 })
